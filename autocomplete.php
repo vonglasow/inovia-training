fcc5bc8eec07eac3ba296fb891fbb2e56d1500fb
Alexis von Glasow <vonglasow@gmail.com>
2014-04-04 10:26:31 +0200

    Initial import

---
+++ autocomplete.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'vendor/autoload.php';
+
+echo 'PHP 5.5 require to works correctly' . PHP_EOL;
+
+echo 'their values are available for autocomplete' . PHP_EOL;
+
+$autocomplete = array(
+    'hoa',
+    'console',
+    'readline',
+    'password',
+    'awesome',
+    'inovia',
+    'git status',
+);
+
+print_r($autocomplete);
+
+$rl = new Hoa\Console\Readline\Readline();
+$rl->setAutocompleter(new Hoa\Console\Readline\Autocompleter\Word($autocomplete));
+var_dump($rl->readLine('> '));
---
+++ try-me.php
@@ -0,0 +1,52 @@
+<?php
+
+require 'vendor/autoload.php';
+
+echo 'PHP 5.5 require to works correctly' . PHP_EOL;
+
+echo 'abcdef', "\n",
+     'ghijkl', "\n",
+     'mnopqr', "\n",
+     'stuvwx';
+
+sleep(1);
+Hoa\Console\Cursor::move('↑');
+sleep(1);
+Hoa\Console\Cursor::move('↑ ←');
+sleep(1);
+Hoa\Console\Cursor::move('←', 3);
+
+sleep(1);
+
+Hoa\Console\Cursor::clear('↔');
+echo 'hello world';
+
+// Save cursor position.
+Hoa\Console\Cursor::save();
+sleep(1);
+
+// Go to the begining of the line.
+Hoa\Console\Cursor::move('←', strlen('hello world'));
+sleep(1);
+
+// Replace “h” by “H”.
+echo 'H';
+sleep(1);
+
+// Go to “w”.
+Hoa\Console\Cursor::move('→', 5);
+sleep(1);
+
+// Replace “w” by “W”.
+echo 'W';
+sleep(1);
+
+// Back to the saved position.
+Hoa\Console\Cursor::restore();
+sleep(1);
+
+echo '!';
+
+sleep(1);
+
+Hoa\Console\Cursor::clear('↔');
